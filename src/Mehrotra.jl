d55977c437df2e185c7bc0900646266cdc619ab7
simon-lc <simonlc@stanford.edu>
2022-04-30 21:13:47 -0700

    add license

--- src/Mehrotra.jl
+++ src/Mehrotra.jl
@@ -1,5 +1,12 @@
 module Mehrotra
 
-greet() = print("Hello World!")
+using LinearAlgebra
+using StaticArrays
+using Symbolics
+using Rotations
+using SparseArrays
+using MeshCat
+using GeometryBasics
+using Plots
 
 end # module
--- src/dev.jl
+++ src/dev.jl
@@ -5,6 +5,15 @@ Pkg.add("StaticArrays")
 Pkg.add("Symbolics")
 Pkg.add("Rotations")
 Pkg.add("SparseArrays")
-
+Pkg.add("MeshCat")
+Pkg.add("GeometryBasics")
+Pkg.add("Plots")
 
 using LinearAlgebra
+using StaticArrays
+using Symbolics
+using Rotations
+using SparseArrays
+using MeshCat
+using GeometryBasics
+using Plots
