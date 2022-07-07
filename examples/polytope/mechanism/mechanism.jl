b63c3d4ff569b886f6d9aad9487db4f37e71b785
simon-lc <simonlc@stanford.edu>
2022-07-07 08:12:59 -0400

    first sim with full codegen

--- examples/polytope/mechanism/mechanism.jl
+++ examples/polytope/mechanism/mechanism.jl
@@ -22,8 +22,20 @@
 #
 # function get_next_state!(mechanism::Mechanism170{T})
 # end
+using Polyhedra
+using MeshCat
+using RobotVisualizer
+using StaticArrays
+using Quaternions
+
+
+vis = Visualizer()
+render(vis)
 
 include("../contact_model/lp_2d.jl")
+include("../visuals.jl")
+include("../rotate.jl")
+include("../quaternion.jl")
 
 mutable struct NodeIndices170
     e::Vector{Int}
@@ -428,121 +440,6 @@ function indexing!(nodes::Vector)
     end
     return nothing
 end
-
-
-mech = Mechanism170(bodies, contacts)
-solver = mech.solver
-
-bodies[1].pose .= [+10,0,0.0]
-bodies[2].pose .= [-10,0,0.0]
-
-θb1 = get_parameters(bodies[1])
-θb2 = get_parameters(bodies[2])
-θc1 = get_parameters(contacts[1])
-parameters = [θb1; θb2; θc1]
-
-solver.parameters .= parameters
-evaluate!(solver.problem,
-    solver.methods,
-    solver.cone_methods,
-    solver.solution,
-    solver.parameters,
-    equality_constraint=true,
-    equality_jacobian_variables=true,
-    equality_jacobian_parameters=true,
-    cone_constraint=true,
-    cone_jacobian=true,
-    cone_jacobian_inverse=true,
-    )
-
-function residual(variables, parameters, bodies, contacts, contact_methods)
-    num_equality = 7
-    num_variables = length(variables)
-
-    x = variables
-    θ = parameters
-
-    e = zeros(num_equality)
-    for body in bodies
-        body_residual!(e, x, θ, body)
-    end
-    for contact in contacts
-        # update xl = [ϕ, pa, pb, N, ∂pa, ∂pb]
-        contact_solver = contact_methods[1].contact_solver
-        xl = contact_methods[1].subvariables
-        θl = contact_methods[1].subparameters
-        contact_methods[1].set_subparameters!(θl, x, θ)
-        update_subvariables!(xl, θl, contact_solver)
-        contact_residual!(e, x, xl, θ, contact, bodies[1], bodies[2])
-    end
-    return e
-end
-
-
-solver.solution.all .= [+0,0,0, -0,0,0, 1e-7, 10.5]
-evaluate!(solver.problem,
-    solver.methods,
-    solver.cone_methods,
-    solver.solution,
-    solver.parameters,
-    equality_constraint=true,
-    equality_jacobian_variables=true,
-    equality_jacobian_parameters=true,
-    cone_constraint=true,
-    cone_jacobian=true,
-    cone_jacobian_inverse=true,
-    )
-
-e0 = residual(solver.solution.all, solver.parameters, bodies, contacts, methods0[3:3])
-e1 = deepcopy(solver.problem.equality_constraint)
-e0
-e1
-norm(e0 - e1, Inf)
-
-J0 = FiniteDiff.finite_difference_jacobian(
-    variables -> residual(variables, solver.parameters, mech.bodies, mech.contacts,
-        methods0[3:3]), mech.solver.solution.all)
-J1 = deepcopy(mech.solver.problem.equality_jacobian_variables)
-norm(J0 - J1, Inf)
-
-mech.bodies[1].pose
-mech.bodies[2].pose
-
-
-using Plots
-plot(Gray.(1e3abs.(J0)))
-plot(Gray.(1e3abs.(J1)))
-plot(Gray.(1e3abs.(J1 - J0)))
-
-J0[3,:]
-J1[3,:]
-J0[6,:]
-J1[6,:]
-
-
-# function finite_diff_jacobian(solver)
-#     evaluate!(solver.problem,
-#         solver.methods,
-#         solver.cone_methods,
-#         solver.solution,
-#         solver.parameters,
-#         equality_constraint=true,
-#         equality_jacobian_variables=true,
-#         equality_jacobian_parameters=true,
-#         cone_constraint=true,
-#         cone_jacobian=true,
-#         cone_jacobian_inverse=true,
-#         )
-#
-#
-#     return
-
-parameter_dimension(bodies[1])
-parameter_dimension(contacts[1])
-mech.solver.options.verbose = false
-@benchmark $solve!($mech.solver)
-Main.@profiler [solve!(mech.solver) for i=1:1000]
-
 
 ################################################################################
 # methods
@@ -1083,6 +980,207 @@ Main.@code_warntype evaluate!(problem,
 
 
 
+
+
+
+
+
+
+
+
+
+################################################################################
+# test mechanism
+################################################################################
+mech = Mechanism170(bodies, contacts)
+solver = mech.solver
+
+bodies[1].pose .= [+10,0,0.0]
+bodies[2].pose .= [-10,0,0.0]
+
+θb1 = get_parameters(bodies[1])
+θb2 = get_parameters(bodies[2])
+θc1 = get_parameters(contacts[1])
+parameters = [θb1; θb2; θc1]
+
+solver.parameters .= parameters
+evaluate!(solver.problem,
+    solver.methods,
+    solver.cone_methods,
+    solver.solution,
+    solver.parameters,
+    equality_constraint=true,
+    equality_jacobian_variables=true,
+    equality_jacobian_parameters=true,
+    cone_constraint=true,
+    cone_jacobian=true,
+    cone_jacobian_inverse=true,
+    )
+
+function residual(variables, parameters, bodies, contacts, contact_methods)
+    num_equality = 7
+    num_variables = length(variables)
+
+    x = variables
+    θ = parameters
+
+    e = zeros(num_equality)
+    for body in bodies
+        body_residual!(e, x, θ, body)
+    end
+    for contact in contacts
+        # update xl = [ϕ, pa, pb, N, ∂pa, ∂pb]
+        contact_solver = contact_methods[1].contact_solver
+        xl = contact_methods[1].subvariables
+        θl = contact_methods[1].subparameters
+        contact_methods[1].set_subparameters!(θl, x, θ)
+        update_subvariables!(xl, θl, contact_solver)
+        contact_residual!(e, x, xl, θ, contact, bodies[1], bodies[2])
+    end
+    return e
+end
+
+
+solver.solution.all .= [+0,0,0, -0,0,0, 1e-7, 10.5]
+evaluate!(solver.problem,
+    solver.methods,
+    solver.cone_methods,
+    solver.solution,
+    solver.parameters,
+    equality_constraint=true,
+    equality_jacobian_variables=true,
+    equality_jacobian_parameters=true,
+    cone_constraint=true,
+    cone_jacobian=true,
+    cone_jacobian_inverse=true,
+    )
+
+e0 = residual(solver.solution.all, solver.parameters, bodies, contacts, methods0[3:3])
+e1 = deepcopy(solver.problem.equality_constraint)
+e0
+e1
+norm(e0 - e1, Inf)
+
+J0 = FiniteDiff.finite_difference_jacobian(
+    variables -> residual(variables, solver.parameters, mech.bodies, mech.contacts,
+        methods0[3:3]), mech.solver.solution.all)
+J1 = deepcopy(mech.solver.problem.equality_jacobian_variables)
+norm(J0 - J1, Inf)
+
+mech.bodies[1].pose
+mech.bodies[2].pose
+
+
+using Plots
+plot(Gray.(1e3abs.(J0)))
+plot(Gray.(1e3abs.(J1)))
+plot(Gray.(1e3abs.(J1 - J0)))
+
+J0[3,:]
+J1[3,:]
+J0[6,:]
+J1[6,:]
+
+
+# function finite_diff_jacobian(solver)
+#     evaluate!(solver.problem,
+#         solver.methods,
+#         solver.cone_methods,
+#         solver.solution,
+#         solver.parameters,
+#         equality_constraint=true,
+#         equality_jacobian_variables=true,
+#         equality_jacobian_parameters=true,
+#         cone_constraint=true,
+#         cone_jacobian=true,
+#         cone_jacobian_inverse=true,
+#         )
+#
+#
+#     return
+
+parameter_dimension(bodies[1])
+parameter_dimension(contacts[1])
+mech.solver.options.verbose = false
+@benchmark $solve!($mech.solver)
+Main.@profiler [solve!(mech.solver) for i=1:1000]
+
+
+
+
+
+################################################################################
+# test simulation
+################################################################################
+
+mech = Mechanism170(bodies, contacts)
+mech.solver.methods.methods[3].contact_solver.solver.options.complementarity_tolerance=1e-2
+mech.solver.methods.methods[3].contact_solver.solver.options.residual_tolerance=1e-10
+# mech.solver.methods.methods[3].contact_solver.solver.options.verbose=true
+Xa2 = [[+1,1,0.0]]
+Xb2 = [[-1,1,0.0]]
+Va15 = [[-2,0,0.0]]
+Vb15 = [[+0,0,0.0]]
+Pp = []
+Pc = []
+iter = []
+
+H = 60
+for i = 1:H
+    mech.bodies[1].pose .= Xa2[end]
+    mech.bodies[1].velocity .= Va15[end]
+    mech.bodies[2].pose .= Xb2[end]
+    mech.bodies[2].velocity .= Vb15[end]
+
+    θb1 = get_parameters(mech.bodies[1])
+    θb2 = get_parameters(mech.bodies[2])
+    θc1 = get_parameters(mech.contacts[1])
+    mech.parameters .= [θb1; θb2; θc1]
+    mech.solver.parameters .= [θb1; θb2; θc1]
+
+    # mech = Mechanism170(bodies, contacts)
+    solve!(mech.solver)
+    va25 = deepcopy(mech.solver.solution.all[1:3])
+    vb25 = deepcopy(mech.solver.solution.all[4:6])
+    push!(Va15, va25)
+    push!(Vb15, vb25)
+    push!(Xa2, Xa2[end] + timestep * va25)
+    push!(Xb2, Xb2[end] + timestep * vb25)
+    p_parent = deepcopy(mech.solver.methods.methods[3].subvariables[2:3]) + (Xa2[end][1:2] + Xb2[end][1:2])/2
+    p_child = deepcopy(mech.solver.methods.methods[3].subvariables[4:5]) + (Xa2[end][1:2] + Xb2[end][1:2])/2
+    push!(Pp, p_parent)
+    push!(Pc, p_child)
+    push!(iter, mech.solver.trace.iterations)
+end
+
+scatter(iter)
+
+################################################################################
+# visualization
+################################################################################
+set_floor!(vis)
+set_background!(vis)
+set_light!(vis)
+
+build_2d_polyhedron!(vis, Ap, bp, name=:polya, color=RGBA(0.2,0.2,0.2,0.7))
+build_2d_polyhedron!(vis, Ac, bc, name=:polyb, color=RGBA(0.9,0.9,0.9,0.7))
+
+setobject!(vis[:contact],
+    HyperSphere(GeometryBasics.Point(0,0,0.), 0.05),
+    MeshPhongMaterial(color=RGBA(1,0,0,1.0)))
+
+anim = MeshCat.Animation(Int(floor(1/timestep)))
+for i = 1:H
+    atframe(anim, i) do
+        set_2d_polyhedron!(vis, Xa2[i][1:2], Xa2[i][3:3], name=:polya)
+        set_2d_polyhedron!(vis, Xb2[i][1:2], Xb2[i][3:3], name=:polyb)
+        settransform!(vis[:contact], MeshCat.Translation(SVector{3}(0, Pp[i]...)))
+    end
+end
+MeshCat.setanimation!(vis, anim)
+render(vis)
+# open(vis)
+# convert_frames_to_video_and_gif("polytope_thrashing_collision")
 
 # n = 2
 # m = 5
