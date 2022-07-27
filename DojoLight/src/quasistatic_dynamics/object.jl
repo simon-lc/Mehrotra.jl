fcc8dc478e20dc52d209481a3aa9b0b66bac9fdc
simon-lc <simonlc@stanford.edu>
2022-07-27 11:08:17 -0400

    restructuring DojoLight

--- DojoLight/src/quasistatic_dynamics/object.jl
+++ DojoLight/src/quasistatic_dynamics/object.jl
@@ -1,31 +1,32 @@
 ################################################################################
 # body
 ################################################################################
-struct Body1160{T,D} <: Node{T}
+struct QuasistaticObject1160{T,D} <: Node{T}
     name::Symbol
     index::NodeIndices1160
     pose::Vector{T}
-    velocity::Vector{T}
+    # velocity::Vector{T}
     input::Vector{T}
     gravity::Vector{T}
     timestep::Vector{T}
     mass::Vector{T}
     inertia::Matrix{T}
+    # stiffness::Vector{T}
     shapes::Vector
 end
 
-function Body1160(timestep::T, mass, inertia::Matrix,
+function QuasistaticObject1160(timestep::T, mass, inertia::Matrix,
         shapes::Vector;
         gravity=-9.81,
         name::Symbol=:body,
         index::NodeIndices1160=NodeIndices1160()) where T
 
     D = 2
-    return Body1160{T,D}(
+    return QuasistaticObject1160{T,D}(
         name,
         index,
         zeros(D+1),
-        zeros(D+1),
+        # zeros(D+1),
         zeros(D+1),
         [gravity],
         [timestep],
@@ -35,48 +36,47 @@ function Body1160(timestep::T, mass, inertia::Matrix,
     )
 end
 
-primal_dimension(body::Body1160{T,D}) where {T,D} = 3
-cone_dimension(body::Body1160{T,D}) where {T,D} = 0
-variable_dimension(body::Body1160{T,D}) where {T,D} = primal_dimension(body) + 2 * cone_dimension(body)
-optimality_dimension(body::Body1160{T,D}) where {T,D} = primal_dimension(body)
-slackness_dimension(body::Body1160{T,D}) where {T,D} = cone_dimension(body)
-equality_dimension(body::Body1160{T,D}) where {T,D} = optimality_dimension(body) + slackness_dimension(body)
+primal_dimension(body::QuasistaticObject1160{T,D}) where {T,D} = 3
+cone_dimension(body::QuasistaticObject1160{T,D}) where {T,D} = 0
 
-function parameter_dimension(body::Body1160{T,D}) where {T,D}
+function parameter_dimension(body::QuasistaticObject1160{T,D}) where {T,D}
     @assert D == 2
     nq = 3 # configuration
-    nv = 3 # velocity
+    # nv = 3 # velocity
     nu = 3 # input
     n_gravity = 1 # mass
     n_timestep = 1 # mass
     n_mass = 1 # mass
     n_inertia = 1 # inertia
-    nθ = nq + nv + nu + n_gravity + n_timestep + n_mass + n_inertia
+    # nθ = nq + nv + nu + n_gravity + n_timestep + n_mass + n_inertia
+    nθ = nq + nu + n_gravity + n_timestep + n_mass + n_inertia
     return nθ
 end
 
-function unpack_variables(x::Vector, body::Body1160{T}) where T
+function unpack_variables(x::Vector, body::QuasistaticObject1160{T}) where T
     return x
 end
 
-function get_parameters(body::Body1160{T,D}) where {T,D}
+function get_parameters(body::QuasistaticObject1160{T,D}) where {T,D}
     @assert D == 2
     pose = body.pose
-    velocity = body.velocity
+    # velocity = body.velocity
     input = body.input
 
     gravity = body.gravity
     timestep = body.timestep
     mass = body.mass
     inertia = body.inertia
-    θ = [pose; velocity; input; gravity; timestep; mass; inertia[1]]
+    # θ = [pose; velocity; input; gravity; timestep; mass; inertia[1]]
+    θ = [pose; input; gravity; timestep; mass; inertia[1]]
     return θ
 end
 
-function set_parameters!(body::Body1160{T,D}, θ) where {T,D}
-    pose, velocity, input, timestep, gravity, mass, inertia = unpack_parameters(θ, body)
+function set_parameters!(body::QuasistaticObject1160{T,D}, θ) where {T,D}
+    # pose, velocity, input, timestep, gravity, mass, inertia = unpack_parameters(θ, body)
+    pose, input, timestep, gravity, mass, inertia = unpack_parameters(θ, body)
     body.pose .= pose
-    body.velocity .= velocity
+    # body.velocity .= velocity
     body.input .= input
 
     body.gravity .= gravity
@@ -86,39 +86,43 @@ function set_parameters!(body::Body1160{T,D}, θ) where {T,D}
     return nothing
 end
 
-function unpack_parameters(θ::Vector, body::Body1160{T,D}) where {T,D}
+function unpack_parameters(θ::Vector, body::QuasistaticObject1160{T,D}) where {T,D}
     @assert D == 2
     off = 0
     pose = θ[off .+ (1:D+1)]; off += D+1
-    velocity = θ[off .+ (1:D+1)]; off += D+1
+    # velocity = θ[off .+ (1:D+1)]; off += D+1
     input = θ[off .+ (1:D+1)]; off += D+1
 
     gravity = θ[off .+ (1:1)]; off += 1
     timestep = θ[off .+ (1:1)]; off += 1
     mass = θ[off .+ (1:1)]; off += 1
     inertia = θ[off .+ 1] * ones(1,1); off += 1
-    return pose, velocity, input, timestep, gravity, mass, inertia
+    # return pose, velocity, input, timestep, gravity, mass, inertia
+    return pose, input, timestep, gravity, mass, inertia
 end
 
-function find_body(bodies::AbstractVector{<:Body1160}, name::Symbol)
+function find_body(bodies::AbstractVector{<:QuasistaticObject1160}, name::Symbol)
     idx = findfirst(x -> x == name, getfield.(bodies, :name))
     return bodies[idx]
 end
 
-function residual!(e, x, θ, body::Body1160)
+function residual!(e, x, θ, body::QuasistaticObject1160)
     index = body.index
     # variables = primals = velocity
     v25 = unpack_variables(x[index.variables], body)
     # parameters
-    p2, v15, u, timestep, gravity, mass, inertia = unpack_parameters(θ[index.parameters], body)
+    # p2, v15, u, timestep, gravity, mass, inertia = unpack_parameters(θ[index.parameters], body)
+    p2, u, timestep, gravity, mass, inertia = unpack_parameters(θ[index.parameters], body)
     # integrator
-    p1 = p2 - timestep[1] * v15
+    # p1 = p2 - timestep[1] * v15
     p3 = p2 + timestep[1] * v25
 
     # mass matrix
     M = Diagonal([mass[1]; mass[1]; inertia[1]])
+
+    K = Diagonal(stiffness)
     # dynamics
-    optimality = M * (p3 - 2*p2 + p1)/timestep[1] - timestep[1] * [0; mass .* gravity; 0] - u * timestep[1];
+    optimality = M * v25 - timestep[1] * [0; mass .* gravity; 0] - u * timestep[1];
     e[index.optimality] .+= optimality
     return nothing
 end
---
+++ DojoLight/src/quasistatic_dynamics/robot.jl
@@ -0,0 +1,133 @@
+################################################################################
+# body
+################################################################################
+struct QuasistaticRobot1160{T,D} <: Node{T}
+    name::Symbol
+    index::NodeIndices1160
+    pose::Vector{T}
+    # velocity::Vector{T}
+    input::Vector{T}
+    gravity::Vector{T}
+    timestep::Vector{T}
+    mass::Vector{T}
+    inertia::Matrix{T}
+    stiffness::Vector{T}
+    shapes::Vector
+end
+
+function QuasistaticRobot1160(timestep::T, mass, inertia::Matrix,
+        shapes::Vector;
+        gravity=-9.81,
+        stiffness=[mass, mass, inertia[1]],
+        name::Symbol=:body,
+        index::NodeIndices1160=NodeIndices1160()) where T
+
+    D = 2
+    return QuasistaticRobot1160{T,D}(
+        name,
+        index,
+        zeros(D+1),
+        # zeros(D+1),
+        zeros(D+1),
+        [gravity],
+        [timestep],
+        [mass],
+        inertia,
+        stiffness,
+        shapes,
+    )
+end
+
+primal_dimension(body::QuasistaticRobot1160{T,D}) where {T,D} = 3
+cone_dimension(body::QuasistaticRobot1160{T,D}) where {T,D} = 0
+
+function parameter_dimension(body::QuasistaticRobot1160{T,D}) where {T,D}
+    @assert D == 2
+    nq = 3 # configuration
+    # nv = 3 # velocity
+    nu = 3 # input
+    n_gravity = 1 # mass
+    n_timestep = 1 # mass
+    n_mass = 1 # mass
+    n_inertia = 1 # inertia
+    n_stiffness = 3
+    # nθ = nq + nv + nu + n_gravity + n_timestep + n_mass + n_inertia
+    nθ = nq + nu + n_gravity + n_timestep + n_mass + n_inertia + n_stiffness
+    return nθ
+end
+
+function unpack_variables(x::Vector, body::QuasistaticRobot1160{T}) where T
+    return x
+end
+
+function get_parameters(body::QuasistaticRobot1160{T,D}) where {T,D}
+    @assert D == 2
+    pose = body.pose
+    # velocity = body.velocity
+    input = body.input
+
+    gravity = body.gravity
+    timestep = body.timestep
+    mass = body.mass
+    inertia = body.inertia
+    stiffness = body.stiffness
+    # θ = [pose; velocity; input; gravity; timestep; mass; inertia[1]]
+    θ = [pose; input; gravity; timestep; mass; inertia[1]; stiffness]
+    return θ
+end
+
+function set_parameters!(body::QuasistaticRobot1160{T,D}, θ) where {T,D}
+    # pose, velocity, input, timestep, gravity, mass, inertia = unpack_parameters(θ, body)
+    pose, input, timestep, gravity, mass, inertia, stiffness = unpack_parameters(θ, body)
+    body.pose .= pose
+    # body.velocity .= velocity
+    body.input .= input
+
+    body.gravity .= gravity
+    body.timestep .= timestep
+    body.mass .= mass
+    body.inertia .= inertia
+    body.stiffness .= stiffness
+    return nothing
+end
+
+function unpack_parameters(θ::Vector, body::QuasistaticRobot1160{T,D}) where {T,D}
+    @assert D == 2
+    off = 0
+    pose = θ[off .+ (1:D+1)]; off += D+1
+    # velocity = θ[off .+ (1:D+1)]; off += D+1
+    input = θ[off .+ (1:D+1)]; off += D+1
+
+    gravity = θ[off .+ (1:1)]; off += 1
+    timestep = θ[off .+ (1:1)]; off += 1
+    mass = θ[off .+ (1:1)]; off += 1
+    inertia = θ[off .+ 1] * ones(1,1); off += 1
+    stiffness = θ[off .+ (1:D+1)]; off += D+1
+    # return pose, velocity, input, timestep, gravity, mass, inertia
+    return pose, input, timestep, gravity, mass, inertia, stiffness
+end
+
+function find_body(bodies::AbstractVector{<:QuasistaticRobot1160}, name::Symbol)
+    idx = findfirst(x -> x == name, getfield.(bodies, :name))
+    return bodies[idx]
+end
+
+function residual!(e, x, θ, body::QuasistaticRobot1160)
+    index = body.index
+    # variables = primals = velocity
+    v25 = unpack_variables(x[index.variables], body)
+    # parameters
+    # p2, v15, u, timestep, gravity, mass, inertia = unpack_parameters(θ[index.parameters], body)
+    p2, u, timestep, gravity, mass, inertia, stiffness = unpack_parameters(θ[index.parameters], body)
+    # integrator
+    # p1 = p2 - timestep[1] * v15
+    p3 = p2 + timestep[1] * v25
+
+    # mass matrix
+    K = Diagonal(stiffness)
+    # dynamics
+    # optimality = M * (p3 - 2*p2 + p1)/timestep[1] - timestep[1] * [0; mass .* gravity; 0] - u * timestep[1];
+    optimality = timestep[1] * K * (p3 - u) - timestep[1] * [0; mass .* gravity; 0];
+    e[index.optimality] .+= optimality
+    return nothing
+end
