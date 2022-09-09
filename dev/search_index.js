64d96a7cab75b27babae8bec371bcb8e43db0162
Documenter.jl <documenter@juliadocs.github.io>
2022-09-09 15:25:43 +0000

    build based on 27b5027

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#Mehrotra","page":"Introduction","title":"Mehrotra","text":"","category":"section"},{"location":"index.html","page":"Introduction","title":"Introduction","text":"Welcome to the documentation for the Mehrotra solver!","category":"page"},{"location":"index.html","page":"Introduction","title":"Introduction","text":"","category":"page"}]
+}
