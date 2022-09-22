1e21f9d65a106888b134b6153aef8e9f081c8d5f
simon-lc <simonlc@stanford.edu>
2022-09-22 18:39:22 -0400

    google submission

--- SymbolicNet/src/todo.jl
+++
@@ -1,8 +0,0 @@
-
-
-################################################################################
-# TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-################################################################################
-# TODO need to capture several variables at once in the macro
-# TODO implement hessian vector product, maybe we can reuse the jacobian infrastructure once again
-# TODO sparse partial jacobians, with code generated jvp matrix multiply
